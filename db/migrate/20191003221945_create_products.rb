b886a332d5346a5b1ee6b85481d4e491b6a3f97e
Jeremy Wiebe <jeremyw76@live.com>
2019-10-03 22:28:15 -0500

    Added product validation
    - Validation must ensure that title, price and stock_quantity are
    present.

--- db/migrate/20191003221945_create_products.rb
+++ db/migrate/20191003221945_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string, :title
-      t.text, :description
-      t.decimal, :price
+      t.string :title
+      t.text :description
+      t.decimal :price
       t.integer :stock_quantity
 
       t.timestamps
