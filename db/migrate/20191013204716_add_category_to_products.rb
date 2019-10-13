5ed28ed95a1cd84eaadfe6e6c1fefb54009b62b4
Jeremy Wiebe <jeremyw76@live.com>
2019-10-13 16:23:33 -0500

    Fixed migration for products foreign key
    - SQLite has a glitch where attempting to add a new column with null not
    allowed fails, even for a table with no data in it.
    - Removed the not null constraint for the add_reference call, and added
    a call to change_column_null after the FK is created.
    - Ran the migration.

--- db/migrate/20191013204716_add_category_to_products.rb
+++ db/migrate/20191013204716_add_category_to_products.rb
@@ -1,5 +1,6 @@
 class AddCategoryToProducts < ActiveRecord::Migration[6.0]
   def change
-    add_reference :products, :category, null: false, foreign_key: true
+    add_reference :products, :category, foreign_key: true
+    change_column_null :products, :category_id, false
   end
 end
