f701eaef10958f579ab1884990b14e48c86b0139
Jeremy Wiebe <jeremyw76@live.com>
2019-10-13 16:39:33 -0500

    Seeded data from csv
    - Rewrote seeder to populate db from a csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-676.times do
-  title = Faker::Commerce.product_name
-  price = Faker::Commerce.price
-  stock_quantiy = Faker::Number.digit
+Product.destroy_all
+Category.destroy_all
 
-  p = Product.create(title: title, description: 'For catchinig flies.', price: price, stock_quantity: stock_quantiy)
+csv_file = Rails.root + 'db/products.csv'
+
+products = SmarterCSV.process(csv_file)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product[:category])
+
+  new_product = category.products.build(title: product[:name], price: product[:price], description: product[:description], stock_quantity: product[:stock_quantity])
+  unless new_product.save then
+    new_product.errors.messages.each do |column, errors|
+      errors.each do |error|
+        puts "The #{column} property #{error}."
+      end
+    end
+  end
 end
